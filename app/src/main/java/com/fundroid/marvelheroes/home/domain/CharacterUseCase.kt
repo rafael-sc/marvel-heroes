dbe0a88dd0abf7db764662819fe52c03d0f544f3
Rafael Costa <rafael.costa.ti@gmail.com>
2020-04-12 22:48:39 -0300

    removing unnecessary code

--- app/src/main/java/com/fundroid/marvelheroes/home/domain/CharacterUseCase.kt
+++ app/src/main/java/com/fundroid/marvelheroes/home/domain/CharacterUseCase.kt
@@ -3,31 +3,14 @@ package com.fundroid.marvelheroes.home.domain
 import com.fundroid.marvelheroes.data.CharacterDetailResponseResult
 import com.fundroid.marvelheroes.data.CharacterListResponseResult
 import com.fundroid.marvelheroes.home.CharactersRepository
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 class CharacterUseCase(
     private val charactersRepository: CharactersRepository
 ) {
-    lateinit var characters: CharacterListResponseResult
-    lateinit var character: CharacterDetailResponseResult
-
     suspend fun getCharactersList(currentPage: Int): CharacterListResponseResult {
-        coroutineScope {
-            launch {
-                characters = charactersRepository.getCharacters(currentPage)
-            }
-        }
-        return characters
+        return charactersRepository.getCharacters(currentPage)
     }
-
     suspend fun getCharacterDetail(characterId: Int): CharacterDetailResponseResult {
-
-        coroutineScope {
-            launch {
-                character = charactersRepository.getCharacterDetail(characterId)
-            }
-        }
-        return character
+        return charactersRepository.getCharacterDetail(characterId)
     }
 }
